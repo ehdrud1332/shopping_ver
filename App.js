5b2d1657316a9bad0de6e3d3bafa5252ecfa1306
CastleJaeMin <ehdrud1332@gmail.com>
2020-10-27 13:26:35 +0900

    AppLoading cacheFonts & Images

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Image} from 'react-native';
+import * as Font from 'expo-font';
+import {Asset} from 'expo-asset';
+import {AppLoading} from 'expo';
+import {Ionicons} from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const cacheImages = images =>
+    images.map(image => {
+        if(typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        }
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const cacheFonts = fonts =>
+    fonts.map(font => Font.loadAsync(font))
+
+const App = () => {
+
+    const [isReady, setIsReady] = useState(false);
+
+    const loadAssets = async() => {
+        const images = cacheImages([
+            "https://images.unsplash.com/photo-1571847140471-1d7766e825ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=673&q=80",
+            require("./assets/splash.png")
+        ]);
+        const fontsAssets = cacheFonts([Ionicons.font])
+        await Promise.all([...images, ...fontsAssets])
+    };
+
+    const onFinish = () => setIsReady(true)
+
+    return isReady ? (
+        <View>
+            <Text>Hello world, this is my app.</Text>
+        </View>
+    ) : (
+        <AppLoading
+            startAsync={loadAssets}
+            onFinish={onFinish}
+            onError={console.log}
+        />
+    )
+};
+
+export default App;
